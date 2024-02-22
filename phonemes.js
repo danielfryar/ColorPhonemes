ad20a15560c2f65a3bf1b2433fc426df681eef7d
Daniel Fryar <danielfryar@Daniels-MacBook-Pro.local>
2024-02-21 21:59:59 -0600

    shooting video

--- phonemes.js
+++ phonemes.js
@@ -1,12 +1,12 @@
 let phonemeDictionary = undefined;
 let phonemes = {
   "vowels": ["AA", "AE", "AH", "AO", "AW", "AY", "EH", "ER", "EY", "IH", "IY", "OW", "OY", "UH", "UW"],
-  "semivowels": ["W", "Y"],
-  "aspirates": ["HH"],
-  "liquids": ["L", "R"],
-  "nasals": ["M", "N", "NG"],
-  "affricates": ["CH", "JH"],
-  "fricatives": ["F", "S", "SH", "TH", "V", "Z", "ZH"],
+  "semivowels": ["W", "Y", "HH"],
+  // "aspirates": ["HH"],
+  "liquids_nasals": ["L", "R", "M", "N", "NG"],
+  // "nasals": ["M", "N", "NG"],
+  // "affricates": ["CH", "JH"],
+  "fricatives": ["F", "S", "SH", "TH", "V", "Z", "ZH", "CH", "JH"],
   "stops": ["B", "D", "G", "K", "P", "T"]
 }
 //   {
@@ -57,14 +57,15 @@ let phonemes = {
 //     '
 // ]
 
-fetch('./CMUdict/cmudict-test')
+fetch('./CMUdict/cmudict-0.7b')
 .then(response => response.text())
 .then(text => {
   // phonemeDictionary = response.dictionary;
   loadDictionary(text);
-  document.getElementById("colorize").removeAttribute("disabled");
-  document.getElementById("colorize").innerText = "Colorize!";
-  document.getElementById("colorize").addEventListener("click", function(event){
+  console.log("Dictionary Loaded");
+  document.getElementById("colorize_btn").removeAttribute("disabled");
+  document.getElementById("col_btn_txt").innerText = "Colorize!";
+  document.getElementById("colorize_btn").addEventListener("click", function(event){
     event.preventDefault();
     colorizeText();
   });
@@ -84,10 +85,13 @@ function loadDictionary(dictionaryText) {
 
 function colorizeText() {
   const text = document.getElementById("words").value;
+  const colorText = document.getElementById('colorText');
+  colorText.innerHTML = "";
   text.split(' ').forEach(word=>{
-    const colorText = document.getElementById('colorText')
+    // const colorText = document.getElementById('colorText')
     colorText.innerHTML += colorizeWord(word) + ' '
   })
+  colorText.parentElement.removeAttribute("hidden")
 }
 function colorizeWord(word) {
   //TODO: take care of punctuation
@@ -290,3 +294,131 @@ colorSelects.forEach(element => {
 //         // }
 //     }
 
+visible = {
+  vowels : false,
+  semivowels : false,
+  liquids : false,
+  fricatives : false,
+  stops : false
+}
+
+document.getElementById("vowels_top").addEventListener('click', (e) => {
+  if (e.target === e.currentTarget) {
+    visible.vowels = !visible.vowels;
+    document.querySelectorAll(".vowel-items").forEach(element => {
+      if (visible.vowels) element.removeAttribute("hidden")
+      else element.setAttribute("hidden", true);
+    });
+  }
+});
+document.getElementById("semivowels_top").addEventListener('click', (e) => {
+  if (e.target === e.currentTarget) {
+    visible.semivowels = !visible.semivowels;
+    document.querySelectorAll(".semivowel-items").forEach(element => {
+      if (visible.semivowels) element.removeAttribute("hidden")
+      else element.setAttribute("hidden", true);
+    });
+  }
+});
+document.getElementById("liquids_top").addEventListener('click', (e) => {
+  if (e.target === e.currentTarget) {
+    visible.liquids = !visible.liquids;
+    document.querySelectorAll(".liquid-items").forEach(element => {
+      if (visible.liquids) element.removeAttribute("hidden")
+      else element.setAttribute("hidden", true);
+    });
+  }
+});
+document.getElementById("fricatives_top").addEventListener('click', (e) => {
+  if (e.target === e.currentTarget) {
+    visible.fricatives = !visible.fricatives;
+    document.querySelectorAll(".fric-items").forEach(element => {
+      if (visible.fricatives) element.removeAttribute("hidden")
+      else element.setAttribute("hidden", true);
+    });
+  }
+});
+document.getElementById("stops_top").addEventListener('click', (e) => {
+  if (e.target === e.currentTarget) {
+    visible.stops = !visible.stops;
+    document.querySelectorAll(".stop-items").forEach(element => {
+      if (visible.stops) element.removeAttribute("hidden")
+      else element.setAttribute("hidden", true);
+    });
+  }
+});
+
+document.getElementById("vowels_color_all").addEventListener("change", (e) => {
+  e.stopPropagation()
+  const selectedColor = document.getElementById("vowels_color").value;
+  console.log(selectedColor)
+  phonemes.vowels.forEach(ph => {
+    console.log(".colphon_"+ph +",colphon_"+ph+"0"+",colphon_"+ph+"1"+",colphon_"+ph+"2")
+    document.querySelectorAll(".colphon_"+ph+",.colphon_"+ph+"0"+",.colphon_"+ph+"1"+",.colphon_"+ph+"2").forEach(el => {
+      if (e.currentTarget.checked) {
+        el.style.color = selectedColor;
+      } else {
+        el.style.color = "";
+      }
+    })
+  })
+})
+document.getElementById("semivowels_color_all").addEventListener("change", (e) => {
+  e.stopPropagation()
+  const selectedColor = document.getElementById("semivowels_color").value;
+  console.log(selectedColor)
+  phonemes.semivowels.forEach(ph => {
+    console.log(".colphon_"+ph)
+    document.querySelectorAll(".colphon_"+ph).forEach(el => {
+      if (e.currentTarget.checked) {
+        el.style.color = selectedColor;
+      } else {
+        el.style.color = "";
+      }
+    })
+  })
+})
+document.getElementById("liquids_color_all").addEventListener("change", (e) => {
+  e.stopPropagation()
+  const selectedColor = document.getElementById("liquids_color").value;
+  console.log(selectedColor)
+  phonemes.liquids_nasals.forEach(ph => {
+    document.querySelectorAll(".colphon_"+ph).forEach(el => {
+      if (e.currentTarget.checked) {
+        el.style.color = selectedColor;
+      } else {
+        el.style.color = "";
+      }
+    })
+  })
+})
+document.getElementById("fricatives_color_all").addEventListener("change", (e) => {
+  e.stopPropagation()
+  const selectedColor = document.getElementById("fricatives_color").value;
+  console.log(selectedColor)
+  phonemes.fricatives.forEach(ph => {
+    document.querySelectorAll(".colphon_"+ph).forEach(el => {
+      if (e.currentTarget.checked) {
+        el.style.color = selectedColor;
+      } else {
+        el.style.color = "";
+      }
+    })
+  })
+})
+document.getElementById("stops_color_all").addEventListener("change", (e) => {
+  e.stopPropagation()
+  const selectedColor = document.getElementById("stops_color").value;
+  console.log(selectedColor)
+  phonemes.stops.forEach(ph => {
+    document.querySelectorAll(".colphon_"+ph).forEach(el => {
+      if (e.currentTarget.checked) {
+        // document.getElementById(ph+"_color").disabled = false;
+        el.style.color = document.getElementById(ph+"_color").value;
+      } else {
+        // document.getElementById(ph+"_color").disabled = true;
+        el.style.color = "";
+      }
+    })
+  })
+})
